000b38e960414ff01900eb96776fe2a884d8dcde
Guest4hu <gustavoqwe25@gmail.com>
2025-07-29 11:46:07 -0300

    feat: padronizando javascript Cards Projetos e Processos

--- assets/JS/componentes/card_servico.js
+++ assets/JS/componentes/card_servico.js
@@ -1,36 +1,36 @@
 export function card_servico() {
-    
-    const servico = [
+    const container_servico = document.getElementById("container_cards_servico");
+    const servico_lista = [
         {
             titulo: "Serviços elétricos",
             descricao: "Serviços completos de elétrica para sua residência.",
-    alt_imagem: "Serviço 1",
-    imagem: "assets/img/eletrica.jpg"
-},
-{
-    titulo: "Serviços de construção",
-    descricao: "Serviços completos de construção de forma rápida e segura.",
-    alt_imagem: "Serviço 2",
-    imagem: "assets/img/construçao.jpg"
-},
-{
-    titulo: "Serviços de pintura",
-    descricao: "O melhor serviço de pintura para sua residência.",
-    alt_imagem: "Serviço 3",
-    imagem: "assets/img/pintura.jpg"
-},
-{
-    titulo: "Serviço de gestão",
-        descricao: "A melhor organização sobre a construção do seu negócio e da sua residência.",
-        alt_imagem: "Serviço 4",
-        imagem: "assets/img/gestao.jpg"
-    }
-];
-
-for (item of servico){
-    const container_servico = document.querySelector("cards");
-    container_servico.innerHTML`
-    <div class="card_servico">
+            alt_imagem: "Serviço 1",
+            imagem: "assets/img/eletrica.jpg",
+        },
+        {
+            titulo: "Serviços de construção",
+            descricao: "Serviços completos de construção de forma rápida e segura.",
+            alt_imagem: "Serviço 2",
+            imagem: "assets/img/construçao.jpg",
+        },
+        {
+            titulo: "Serviços de pintura",
+            descricao: "O melhor serviço de pintura para sua residência.",
+            alt_imagem: "Serviço 3",
+            imagem: "assets/img/pintura.jpg",
+        },
+        {
+            titulo: "Serviço de gestão",
+            descricao: "A melhor organização sobre a construção do seu negócio e da sua residência.",
+            alt_imagem: "Serviço 4",
+            imagem: "assets/img/gestao.jpg",
+        }
+    ];
+    for (const item of servico_lista) {
+        const card_servico = document.createElement("div");
+        card_servico.className = "card-servico"
+        card_servico.innerHTML = `
+        <article>
         <figure>
             <img src="${item.imagem}" alt="${item.alt_imagem}" class="servico-imagem" />
             <figcaption class="figcaption-servico">
@@ -38,6 +38,8 @@ for (item of servico){
                 <p>${item.descricao}</p>
             </figcaption>
         </figure>
-    </div>`;
+        </article>
+        `;
+        container_servico.appendChild(card_servico);
     }
 }
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,10 @@
 import {nav_bar} from './componentes/nav_bar.js' 
 import {footer} from './componentes/footer.js' 
 import {card_servico} from './componentes/card_servico.js'
+import { card_projetos } from './componentes/card_projetos.js'
+
 
 nav_bar()
 footer()
 card_servico()
+card_projetos()
