1a2326a9acc593675932fe06a60e064334f8489f
Guest4hu <gustavoqwe25@gmail.com>
2025-07-31 11:47:30 -0300

    feat: Começando a pagina processos

--- assets/JS/componentes/nav_bar.js
+++ assets/JS/componentes/nav_bar.js
@@ -13,7 +13,7 @@ export function nav_bar() {
                 <li><a href="index.html">Início</a></li>
                 <li><a href="servicos.html">Serviços</a></li>
                 <li><a href="projetos.html">Projetos</a></li>
-                <li><a href="#processos">Processos</a></li>
+                <li><a href="processos.html">Processos</a></li>
                 <li><a href="#testemunhos">Testemunhos</a></li>
                 <li><a href="contato">contato</a></li>
             </ul>
