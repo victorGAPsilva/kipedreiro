c29073c8f1267851f2dc066356f5d84d7bee3cb7
Guest4hu <gustavoqwe25@gmail.com>
2025-07-31 10:55:25 -0300

    feat: terminando os arrays e propriedades dentro do HTML atraves do JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,9 +3,13 @@ import {footer} from './componentes/footer.js'
 import {card_servico} from './componentes/card_servico.js'
 import {card_projetos } from './componentes/card_projetos.js'
 import {card_processos} from './componentes/card_processos.js'
+import {testemunho_slides} from './componentes/testemunho_slides.js'
+
+
 
 nav_bar()
 footer()
 card_servico()
 card_projetos()
 card_processos()
+testemunho_slides()
