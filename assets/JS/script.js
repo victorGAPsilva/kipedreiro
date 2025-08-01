34dab4b3bec428664fd5b99eb894eaeba6ea9137
Guest4hu <gustavoqwe25@gmail.com>
2025-08-01 11:40:25 -0300

    feat: Terminando os arrays e propriedades de Projetos junto com a estilização, falta arruma certos erros e estilizar

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,6 +4,8 @@ import {card_servico} from './componentes/card_servico.js'
 import {card_projetos } from './componentes/card_projetos.js'
 import {card_processos} from './componentes/card_processos.js'
 import {testemunho_slides} from './componentes/testemunho_slides.js'
+
+
 nav_bar()
 footer()
 card_servico()
