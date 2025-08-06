42bbc8aec2107432deb054b8134c0d226b16952f
Gustavo Leite de Oliveira <gustavoqwe25@gmail.com>
2025-08-05 22:17:52 -0300

    Fix:Ajustes e arrumando certas funcionalidades, Toques Finais.

--- assets/JS/componentes/footer.js
+++ assets/JS/componentes/footer.js
@@ -1,30 +1,45 @@
 export function footer() {
-    const footer = document.getElementById("footer");
-    footer.innerHTML = ` <div class="container">
-            <div class="coluna-footer">
-                <h3>sobre nos</h3>
-                 <p>A Ki-Pedreiro é uma construtora de São Paulo que realiza reformas e obras com qualidade, agilidade e preço justo. Contamos com uma equipe experiente para transformar seu projeto em realidade.</p>
-            </div>
-            <div class="coluna-footer">
-                <h3>links uteis</h3>
-                <ul>
-                    <li><a href="#hero">Início</a></li>
-                    <li><a href="#servicos">Serviços</a></li>
-                    <li><a href="#projetos">Projetos</a></li>
-                    <li><a href="#processos">Processos</a></li>
-                    <li><a href="contato">contato</a></li>
-                </ul>
-            </div>
-            <div class="coluna-footer">
-                <h3>Siga nas redes sociais</h3>
-                <div class="redes">
-                    <a href="https://instagram.com/kipedreiro" target="_blank"> <img src="assets/img/icon/instagram.svg"
-                            alt="instagram" class="redes-sociais"></a>
+  const footer = document.getElementById("footer");
 
-                    <a href="https://linkedin.com/in/kipedreiro" target="_blank"> <img
-                            src="assets/img/icon/linkedin.svg" alt="linkedin" class="redes-sociais"></a>
-                </div>
+  footer.innerHTML = `
+    <div class="container">
+      <div class="coluna-footer">
+        <h3>Sobre nós</h3>
+        <p>A Ki-Pedreiro é uma construtora de São Paulo que realiza reformas e obras com qualidade, agilidade e preço justo. Contamos com uma equipe experiente para transformar seu projeto em realidade.</p>
+      </div>
 
-            </div>
-        </div>`
+      <div class="coluna-footer">
+        <h3>Links úteis</h3>
+        <ul class="links-footer">
+          <li><a href="index.html">Início</a></li>
+          <li><a href="servicos.html">Serviços</a></li>
+          <li><a href="projetos.html">Projetos</a></li>
+          <li><a href="processos.html">Processos</a></li>
+          <li><a href="contato.html">Contato</a></li>
+        </ul>
+      </div>
+
+      <div class="coluna-footer">
+        <h3>Siga nas redes sociais</h3>
+        <div class="redes">
+          <a href="https://instagram.com/kipedreiro" target="_blank">
+            <img src="assets/img/icon/instagram.svg" alt="Instagram" class="redes-sociais">
+          </a>
+          <a href="https://linkedin.com/in/kipedreiro" target="_blank">
+            <img src="assets/img/icon/linkedin.svg" alt="LinkedIn" class="redes-sociais">
+          </a>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const currentPage = window.location.pathname.split("/").pop(); 
+  const footerLinks = footer.querySelectorAll(".links-footer a");
+
+  footerLinks.forEach(link => {
+    const href = link.getAttribute("href");
+    if (href === currentPage) {
+      link.parentElement.style.display = "none";
+    }
+  });
 }
--- assets/JS/componentes/nav_bar.js
+++ assets/JS/componentes/nav_bar.js
@@ -1,21 +1,33 @@
 export function nav_bar() {
-    const header = document.getElementById("header");
-    header.innerHTML = `  <div class="menu-secundario">
-            <span><img src="assets/img/icon/001-phone-receiver-silhouette.png" alt="telefone de contato"
-                    class="icon">(11) 92152-4829</span>
-            <span> <img src="assets/img/icon/019-email-1.png" alt="email de contato"
-                    class="icon">contato@kipedreiro.com</span>
-        </div>
+  const header = document.getElementById("header");
 
-        <nav class="menu-primario">
-            <a href="index.html"><img src="assets/img/logo/KiPedreiro.png" alt="Kipedreiro logo" class="logo"><a>
-            <ul class="menu-navegacao">
-                <li><a href="index.html">Início</a></li>
-                <li><a href="servicos.html">Serviços</a></li>
-                <li><a href="projetos.html">Projetos</a></li>
-                <li><a href="processos.html">Processos</a></li>
-                <li><a href="contato.html">Contato</a></li>
-            </ul>
-        </nav>`
+  header.innerHTML = `
+    <div class="menu-secundario">
+      <span><img src="assets/img/icon/001-phone-receiver-silhouette.png" alt="telefone de contato" class="icon">(11) 92152-4829</span>
+      <span><img src="assets/img/icon/019-email-1.png" alt="email de contato" class="icon">contato@kipedreiro.com</span>
+    </div>
+
+    <nav class="menu-primario">
+      <a href="index.html"><img src="assets/img/logo/KiPedreiro.png" alt="Kipedreiro logo" class="logo"></a>
+      <ul class="menu-navegacao">
+        <li><a href="index.html">Início</a></li>
+        <li><a href="servicos.html">Serviços</a></li>
+        <li><a href="projetos.html">Projetos</a></li>
+        <li><a href="processos.html">Processos</a></li>
+        <li><a href="contato.html">Contato</a></li>
+      </ul>
+    </nav>
+  `;
+
+  const currentPage = window.location.pathname.split("/").pop();
+
+  const navLinks = document.querySelectorAll(".menu-navegacao a");
+
+  navLinks.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage) {
+      link.parentElement.style.display = "none";
+    }
+  });
 }
-
